60f847e41266892ceb602c948b2e142526863bda
Jeddi212 <jedediahf51@gmail.com>
2022-06-23 22:45:14 +0700

    refactor main.cr

--- src/main.cr
+++ src/main.cr
@@ -1,15 +1,9 @@
-require "./models/*"
-require "../config/config"
-
 require "kemal"
 
-get "/" do
-  # "#{typeof(conn).to_s} ::: OK"
-  todo = Todos.create({title: "New Title 3"})
-  item = Items.create({checked: false, todo_id: 1})
-  puts
-  puts
-  puts todo.inspect
-end
+# Configure
+require "../config/config"
+
+# Controller
+require "./modules/todos/controller/todo_controller"
 
 Kemal.run
--- src/todo_crystal.cr
+++
@@ -1,6 +0,0 @@
-# TODO: Write documentation for `TodoCrystal`
-module TodoCrystal
-  VERSION = "0.1.0"
-
-  # TODO: Put your code here
-end
